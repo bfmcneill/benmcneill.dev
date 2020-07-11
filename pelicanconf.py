597e8539dfd5f498d14bc450bf84dbdc93edf153
Ben McNeill <ben@activeprogrammer.com>
2020-07-11 09:01:00 -0600

    update conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,28 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-PATH = 'content'
+PATH = "content"
 AUTHOR = "Ben McNeill"
 SITENAME = "Ben McNeills's Blog"
 SITETITLE = SITENAME
 
 
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 SITEURL = "http://localhost:8000"
 SITESUBTITLE = "Engineer | Software Developer | Data Scientist"
 
-DISPLAY_PAGES_ON_MENU = True
+# DISPLAY_PAGES_ON_MENU = True
 
 ARTICLE_URL = "{slug}.html"
 ARTICLE_SAVE_AS = "{slug}.html"
 
 STATIC_PATHS = ["img", "files", "html", "extra", "2020"]
+EXTRA_PATH_METADATA = {
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/jm-photo.jpg": {"path": "jm-photo.jpg"},
+    "extra/CNAME": {"path": "CNAME"},
+    "extra/cactus.jpg": {"path": "cactus.jpg"},
+}
 
 GITHUB_CORNER_URL = "https://github.com/bfmcneill/benmcneill.dev"
 
-TIMEZONE = 'America/Denver'
+TIMEZONE = "America/Denver"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 USE_FOLDER_AS_CATEGORY = False
 MAIN_MENU = True
@@ -38,19 +44,23 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ('Podcast', 'https://podcasts.apple.com/us/podcast/profitable-python/id1472548149'),
-         )
+    (
+        "Profitable Python Podcast",
+        "https://podcasts.apple.com/us/podcast/profitable-python/id1472548149",
+    ),
+)
 
 # Social widget
 SOCIAL = (
-    ('twitter', 'https://twitter.com/bfmcneill'),
-    ('linkedin', 'https://www.linkedin.com/in/bfmcneill/'),
-    ('github', 'https://github.com/bfmcneill'),)
+    ("twitter", "https://twitter.com/bfmcneill"),
+    ("linkedin", "https://www.linkedin.com/in/bfmcneill/"),
+    ("github", "https://github.com/bfmcneill"),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 THEME = "./themes/Flex"
 
@@ -60,10 +70,22 @@ PLUGIN_PATHS = [
 
 PLUGINS = [
     "tipue_search",
-    'liquid_tags.img',
-    'liquid_tags.youtube',
+    "liquid_tags.img",
+    "liquid_tags.youtube",
     "autostatic",
+    "pelican-cover-image",
 ]
 
 USE_TIPUE_SEARCH = True
-DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search']
+DIRECT_TEMPLATES = ["index", "tags", "categories", "authors", "archives", "search"]
+
+
+MENUITEMS = (
+    # ("Authors", "/authors.html"),
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+    # ("Sitemap", "/sitemap.xml"),
+    # ('RSS', "feeds/all.atom.xml")
+    # ("Search","/search.html")
+)
